8b715305cdd34b8c5737d93cf823d205897e7224
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2025-12-06 07:36:10 -0500

    add get_connecting_routes

---
+++ get_connecting_routes.py
@@ -0,0 +1,99 @@
+import networkx as nx
+from typing import List
+import osmnx as ox
+import requests
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+import os
+import flexpolyline as fpl
+
+load_dotenv()
+HERE_API_KEY = os.getenv('HERE_API_KEY')
+
+
+def relabel_nodes_in_dfs_order(route_graphs: List[nx.MultiDiGraph]):
+    for i, route_graph in enumerate(route_graphs):
+        start_nodes = [node for node in route_graph.nodes if route_graph.in_degree(node) == 0]
+        assert len(start_nodes) == 1
+        start_node = start_nodes[0]
+        dfs_nodes = nx.dfs_preorder_nodes(route_graph, start_node)
+        new_id_mapping = {old_node_id: (i * 10**6) + j for j, old_node_id in enumerate(dfs_nodes)}
+        route_graph.graph['my_id'] = f'G{i}'
+        nx.relabel_nodes(route_graph, new_id_mapping, copy=False)
+
+def get_connecting_routes(route_graphs: List[nx.MultiDiGraph]):
+    relabel_nodes_in_dfs_order(route_graphs)
+    toll_graph = route_graphs[0]
+    connecting_routes = []
+    for toll_node in toll_graph.nodes:
+        for route_graph in route_graphs[1:]:
+            x, y = toll_graph.nodes[toll_node]['x'], toll_graph.nodes[toll_node]['y']
+            nearest_node = ox.distance.nearest_nodes(route_graph, X=x, Y=y, return_dist=False)
+            assert nearest_node not in toll_graph.nodes and toll_node not in route_graph.nodes # Ensure non-conflicting node ids for when they're merged
+            connecting_routes.append((nearest_node, toll_node))
+            connecting_routes.append((toll_node, nearest_node))
+    return connecting_routes
+
+def get_traffic_aware_durations(route_graphs: List[nx.MultiDiGraph], connections, origin, destination):
+    origin = f'{origin[0]},{origin[1]}'
+    destination = f'{destination[0]},{destination[1]}'
+    polylines = []
+    for route_graph in route_graphs:
+        start_nodes = [node for node in route_graph.nodes if route_graph.in_degree(node) == 0]
+        assert len(start_nodes) == 1
+        start_node = start_nodes[0]
+        dfs_nodes = nx.dfs_preorder_nodes(route_graph, start_node)
+        waypoints = []
+        for node in dfs_nodes:
+            waypoints.append(f'{route_graph.nodes[node]['y']},{route_graph.nodes[node]['x']}')
+
+        departure_time = datetime.now(timezone.utc).isoformat()
+
+        url = "https://router.hereapi.com/v8/routes"
+        params = {
+            "transportMode": "car",
+            "origin": origin,
+            "destination": destination,
+            "via": waypoints,
+            # "alternatives": 2,
+            "return": "summary,polyline,actions",
+            "routingMode": "fast",
+            "departureTime": departure_time,
+            "apiKey": HERE_API_KEY
+        }
+        r = requests.get(url, params=params)
+        # print(r.text)
+        r.raise_for_status()
+        route = r.json()['routes'][0]
+        total = 0
+        full_polyline = []
+        for i, section in enumerate(route['sections']):
+            polyline_str = section['polyline']
+            polyline_sec = fpl.decode(polyline_str)
+            full_polyline += polyline_sec
+
+            total += section['summary']['duration']
+            # print(section['summary']['duration'] / 60)
+            # print(section['summary']['length'])
+        polylines.append(full_polyline)
+
+        print(total / 60)
+        print('end of route\n')
+        # print(len(route['sections']))
+        # print(len(r.json()['routes']))
+
+        connections_from_route = [(u, v) for (u, v) in connections if u in route_graph.nodes]
+        
+    return polylines
+    
+
+
+def build_connected_graph(route_graphs: List[nx.MultiDiGraph], origin, destination):
+    connecting_routes = get_connecting_routes(route_graphs)
+    full_graph = nx.MultiDiGraph(nx.compose_all(route_graphs))
+
+    for u, v in connecting_routes:
+        full_graph.add_edge(u, v)
+
+    return full_graph, connecting_routes
+
---
+++ test_get_connecting_routes.py
@@ -0,0 +1,20 @@
+import folium
+from visualize_graph import setup_folium_graph, visualize_graph
+from test_get_route_graph import test_get_route_graph
+from get_connecting_routes import build_connected_graph, get_traffic_aware_durations
+
+def test_connecting_routes():
+    route_graphs, polylines, origin, destination = test_get_route_graph()
+    full_graph, connecting_routes = build_connected_graph(route_graphs, origin, destination)
+    polylines = get_traffic_aware_durations(route_graphs, connecting_routes, origin, destination)
+    
+    colours = ['green', 'blue', 'purple']
+    m = setup_folium_graph(full_graph)
+    for i, polyline in enumerate(polylines):
+        folium.PolyLine(polyline, color=colours[i], weight=3, opacity=0.8,).add_to(m)
+    # m = visualize_graph(full_graph, m, 'orange', True)
+    m.save('connected_route_graphs.html')
+
+
+if __name__ == '__main__':
+    test_connecting_routes()
