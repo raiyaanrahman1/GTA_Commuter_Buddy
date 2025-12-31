8b619eadbe12bc5ff4a157e487623fe5df175362
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2025-12-31 00:09:04 -0500

    add main.py, test_get_simplified_gta_graph_network.py

---
+++ testing/test_get_simplified_gta_graph_network.py
@@ -0,0 +1,4 @@
+from src.get_simplified_gta_graph_network import get_simplified_gta_graph_network
+
+def test_get_simplified_gta_graph_network():
+    get_simplified_gta_graph_network()  # this is done for consistency with the other test files 
